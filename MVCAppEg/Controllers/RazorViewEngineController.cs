18d7ee98ea3e96dbc70011603f6be0cd8d6b4279
EZLLeno <EZLLeno@EZL-LEN>
2025-08-20 12:39:03 +0530

    comm

--- MVCAppEg/Controllers/RazorViewEngineController.cs
+++ MVCAppEg/Controllers/RazorViewEngineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCAppEg.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,11 @@ namespace MVCAppEg.Controllers
 {
     public class RazorViewEngineController : Controller
     {
+
+        Models.IBM06Aug25DbEntities _db = new Models.IBM06Aug25DbEntities();
+
+
+
         // GET: RazorViewEngine
         public ActionResult BasicSyntaxIntro()
         {
@@ -30,6 +36,43 @@ namespace MVCAppEg.Controllers
 
         public ActionResult NestedLayoutContent()
         {
+            return View();
+        }
+
+
+        public ActionResult ValidationAndHTMLHelpersContent()
+        {
+            ViewBag.DeptID = new SelectList(_db.Depts, "DeptID", "DeptName"); 
+
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ValidationAndHTMLHelpersContent(Employee obj)
+        {
+
+            if (obj.FirstName.Length<=5)
+            {
+                     ModelState.AddModelError("FirstName", "Min Len must be above 5 Char");
+            }
+   
+
+
+
+            if (ModelState.IsValid) {
+
+
+                return RedirectToAction("index");
+
+            
+            }
+
+
+            ViewBag.DeptID = new SelectList(_db.Depts, "DeptID", "DeptName");
+
+
             return View();
         }
 
---
+++ MVCAppEg/Models/EmployeeMetaData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCAppEg.Models
+{
+    public class EmployeeMetaData
+    {
+        [Required(ErrorMessage ="Employee Name must be Email ID and Required")]
+        [StringLength(30)]
+        [DisplayName("Employee First Name")]
+        [DataType(DataType.EmailAddress)]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Employee Last Name")]
+        public string LastName { get; set; }
+        [Required]
+       
+        [Range (10000,1000000)]
+       
+        [DisplayName("Basic Salary")]
+        public decimal Salary { get; set; }
+    }
+
+    [MetadataType(typeof(EmployeeMetaData) )]
+    public partial class Employee
+    { 
+    
+    
+    }
+
+    }
